f32942986e7cec4488a77d53a3e3f1f526f7b1dd
Alexandru Nastase <nastase.alexandru100@gmail.com>
2017-12-18 14:57:12 +0000

    Removing whitespace

--- sketch.js
+++ sketch.js
@@ -3,18 +3,12 @@ let r;
 let n = 0;
 let c = 8;
 
-// comment
-
 function setup() {
     createCanvas(900, 900);
     background(51);
-
 }
 
-
-
 function draw() {
-
     translate(width/2, height/2);
 
     phi = n * 137.5;
@@ -23,11 +17,8 @@ function draw() {
     let x = r * cos(phi);
     let y = r * sin(phi);
 
-
     stroke(0);
     ellipse(x, y, 10, 10);
 
-
     n++;
-
 }
