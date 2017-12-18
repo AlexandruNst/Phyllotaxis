2702a855b05d5bfb831db6882a2547db95b1e78f
Alexandru Nastase <nastase.alexandru100@gmail.com>
2017-12-18 14:28:14 +0000

    Adding to github

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let phi;
+let r;
+let n = 0;
+let c = 4;
+
+function setup() {
+    createCanvas(900, 900);
+    background(51);
+
+}
+
+
+
+function draw() {
+
+    translate(width/2, height/2);
+
+    phi = n * 137.5;
+    r = c * sqrt(n);
+
+    let x = r * cos(phi);
+    let y = r * sin(phi);
+
+
+    stroke(0);
+    ellipse(x, y, 10, 10);
+
+
+    n++;
+
+}
